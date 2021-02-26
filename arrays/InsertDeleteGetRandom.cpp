dfbf10a4f6357d30c435b9ce4363f3c68180a3b3
Ganzorig Ulziibayar <ganzo.ulziibayar@gmail.com>
2021-02-26 16:56:15 -0600

    modified

--- arrays/InsertDeleteGetRandom.cpp
+++ arrays/InsertDeleteGetRandom.cpp
@@ -1,10 +1,6 @@
 // 380. Insert Delete GetRandom O(1)
 // Implement the RandomizedSet class:
 
-//     bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
-//     bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
-//     int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
-
 // Follow up: Could you implement the functions of the class with each function works in average O(1) time?
 
 // used hash map
